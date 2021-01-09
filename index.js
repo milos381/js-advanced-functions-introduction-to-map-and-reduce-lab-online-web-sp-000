f98b1de7de11619459ec1d11b7ce8358fe7d5a7f
milos381 <milos_popovic@outlook.com>
2021-01-09 18:12:33 -0500

    commit tree d66cc5291a218a8b4ed2557a393e3f832edf18e3

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+let numbers = [4, 9, 16, 25];
+function mapToNegativize(array) {
+    return array.map(x => x - x*2);
+}
+function mapToNoChange(array) {
+    let arr = []
+    for (let index = 0; index < array.length; index++) {
+        arr.push(array[index]);
+    }
+    return arr
+}
+function mapToDouble(array) {
+    return array.map(x => x*2);
+}
+function mapToSquare(array) {
+    return array.map(x => x*x);
+}
+function reduceToTotal(array, start=0) {
+    let total = start
+    for (let i = 0; i < array.length; i++ ) {
+      total = total + array[i]
+    }
+    return total
+  }
+  //had to find help for last two. had no idea what is asked from me to do 
+  function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
